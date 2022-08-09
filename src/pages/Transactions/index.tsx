cef33b35fb9d39a4e6d950f92cbc3f5351edaa5d
Wallace Júnior <wflj1997@hotmail.com>
2022-08-09 16:14:00 -0300

    Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,55 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
-import { TransactionsContainer } from "./styles";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 interface TransactionsProps {}
 
 export const Transactions: React.FC<TransactionsProps> = () => (
-  <TransactionsContainer>
+  <div>
     <Header />
     <Summary />
-  </TransactionsContainer>
+
+    <TransactionsContainer>
+      <TransactionsTable>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de site</td>
+
+            <td>
+              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+            </td>
+
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+
+          <tr>
+            <td>Hambúrguer</td>
+
+            <td>
+              <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+            </td>
+
+            <td>Alimentação</td>
+            <td>10/04/2022</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel do apartamento</td>
+
+            <td>
+              <PriceHighlight variant="outcome">- R$ 1.200,00</PriceHighlight>
+            </td>
+
+            <td>Casa</td>
+            <td>27/03/2022</td>
+          </tr>
+        </tbody>
+      </TransactionsTable>
+    </TransactionsContainer>
+  </div>
 );
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,3 +1,45 @@
 import styled from "styled-components";
 
-export const TransactionsContainer = styled.div``;
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding-inline: 1.5rem;
+`;
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  /* margin-top: 1.5rem; */
+
+  td {
+    padding: 1.25rem 2rem;
+    background-color: ${({ theme }) => theme.colors.gray[700]};
+
+    &:first-child {
+      width: 50%;
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
+
+interface PriceHighlightProps {
+  variant: "income" | "outcome";
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${({ theme, variant }) => {
+    if (variant === "income") {
+      return theme.colors.green[300];
+    }
+
+    return theme.colors.red[300];
+  }};
+`;
