4a14d81442436e2fca0ce93bef8b726d2f5c6f32
Wallace Júnior <wflj1997@hotmail.com>
2022-08-09 15:38:52 -0300

    Setup do projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.green[500]};
+  }
+
+  body {
+    -webkit-font-smoothing: antialised;
+
+    ${({ theme }) => css`
+      background-color: ${theme.colors.gray[800]};
+      color: ${theme.colors.gray[100]};
+    `}
+  }
+
+  body, input, textarea, button, select {
+    font: 400 1rem "Roboto", sans-serif;
+  }
+`;
